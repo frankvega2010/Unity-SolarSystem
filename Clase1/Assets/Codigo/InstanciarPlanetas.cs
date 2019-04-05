41a374f6c5f0e5ad803ad5dc7ac59cbefcf74185
Franco <franco_vega@msn.com>
2019-04-05 16:29:50 -0300

    Fixed values for instantiated planets

--- Clase1/Assets/Codigo/InstanciarPlanetas.cs
+++ Clase1/Assets/Codigo/InstanciarPlanetas.cs
@@ -24,8 +24,6 @@ public class InstanciarPlanetas : MonoBehaviour
     {
         if(planeta)
         {
-            float f = 28.0f;
-
             for (int i = 1; i < 9;i++)
             {
                 
@@ -35,27 +33,51 @@ public class InstanciarPlanetas : MonoBehaviour
                 {
                     case 1:
                         escala = new Vector3(1,1,1);
+                        planetaClonado.GetComponent<Planeta>().radio = 9;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 6;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 10;
                         break;
                     case 2:
                         escala = new Vector3(3, 3, 3);
+                        planetaClonado.GetComponent<Planeta>().radio = 18;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 2;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 7;
                         break;
                     case 3:
                         escala = new Vector3(3.5f, 3.5f, 3.5f);
+                        planetaClonado.GetComponent<Planeta>().radio = 36;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 1.7f;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 8;
                         break;
                     case 4:
                         escala = new Vector3(1.8f, 1.8f, 1.8f);
+                        planetaClonado.GetComponent<Planeta>().radio = 50;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 1;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 5;
                         break;
                     case 5:
                         escala = new Vector3(6, 6, 6);
+                        planetaClonado.GetComponent<Planeta>().radio = 70;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 0.7f;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 6;
                         break;
                     case 6:
                         escala = new Vector3(5.5f, 5.5f, 5.5f);
+                        planetaClonado.GetComponent<Planeta>().radio = 90;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 0.5f;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 9;
                         break;
                     case 7:
                         escala = new Vector3(3, 3, 3);
+                        planetaClonado.GetComponent<Planeta>().radio = 110;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 0.3f;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 6;
                         break;
                     case 8:
                         escala = new Vector3(2.9f, 2.9f, 2.9f);
+                        planetaClonado.GetComponent<Planeta>().radio = 130;
+                        planetaClonado.GetComponent<Planeta>().velocidadGiro = 0.18f;
+                        planetaClonado.GetComponent<Planeta>().velocidadRotEje = 4;
                         break;
                     default:
                     break;
@@ -63,16 +85,8 @@ public class InstanciarPlanetas : MonoBehaviour
 
                 planetaClonado.transform.localScale = escala;
                 planetaClonado.GetComponent<MeshRenderer>().material = materialesPlanetas[i];
-                planetaClonado.GetComponent<Planeta>().radio = 8 * i;
-                planetaClonado.GetComponent<Planeta>().velocidadGiro = f / 20;
-                planetaClonado.GetComponent<Planeta>().velocidadRotEje = f / 10;
                 planetaClonado.GetComponent<Planeta>().tieneLuna = false;
                 planetaClonado.GetComponent<Planeta>().tieneAnillo = false;
-                // planetaClonado.GetComponent<Planeta>().sol = sol;
-
-                //t.GetComponent<MeshRenderer>().material = materialesPlanetas[i];
-                // planetaClonado.transform.localScale = new Vector3();
-                f--;
             }
         }
         
